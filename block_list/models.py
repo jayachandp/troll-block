ee65e2be914f05ba2e3ef2746a0d7e52da68bef8
Sahith Chandan <sahith88@gmail.com>
2013-05-30 15:22:25 +0530

    Changed DB and included blocked list
    Removed subscribed list from db
    
    changed ui in blocked_list.html in order to list out blocked users of
    followers. also included the functionality to subscribe and unsubscribe
    the list

--- block_list/models.py
+++ block_list/models.py
@@ -2,26 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-share_choices = (
-    ('public', 'private'),
-    ('private', 'private'),
-    ('no', 'no')
-    )
-
 class Blocked_List(models.Model):
-    user_id = models.CharField(max_length=10)
+    user_id = models.CharField(max_length=30)
     share = models.CharField(max_length=10)
     blocked_users = models.TextField()
-    
-    def __unicode__(self):
-        return "User Id: %s, Blocked Users: %s" %(self.user_id,
-                                                  self.blocked_users)
-
-
-class Subscribed_List(models.Model):
-    user_id = models.ForeignKey(Blocked_List)
     subscribed_users = models.TextField()
+    #def save(self, *args, **kwargs):
+    #    pass
     
     def __unicode__(self):
-        return "User Id: %s, Subscribed Users: %s" %(self.user_id,
-                                                     self.subscribed_users)
+        return "User Id: %s, Shared: %s" %(self.user_id, self.share)
--- block_list/views.py
+++ block_list/views.py
@@ -58,12 +58,20 @@ def home(request):
     if not access_token:
         return HttpResponse("You need an access token!")
     token = oauth.OAuthToken.from_string(access_token)   
+<<<<<<< HEAD
     twit = Twitter(auth=OAuth(token.key, token.secret,
                 CONSUMER_KEY, CONSUMER_SECRET))
     login_id = request.session.get('login_id')
     login_name = request.session.get('login_name')
     login_screen_name = request.session.get('login_screen_name')
     #login_user = twit.users.show(screen_name=twit.account.settings().get('screen_name'))
+=======
+    twit = Twitter(auth=OAuth(token.key,
+                                token.secret,
+                                CONSUMER_KEY, 
+                                CONSUMER_SECRET))
+    login_user = twit.users.show(screen_name=twit.account.settings().get('screen_name'))
+>>>>>>> Changed DB and included blocked list
     #POST method lets you block the selected users
     if request.method == "POST":
         getids = request.POST.getlist('followers')
@@ -75,38 +83,47 @@ def home(request):
         try:
             user_obj = Blocked_List.objects.get(user_id=login_id)
             temp_list = eval(user_obj.blocked_users)
-            for id_ in getids:
-                if id_ not in temp_list:
-                    temp_list.append(id_)
-            user_obj.blocked_users = json.dumps(temp_list)
-            user_obj.save()
         except:
+<<<<<<< HEAD
             user_obj = Blocked_List(user_id=login_id,
                                         share='no')
+=======
+            user_obj = Blocked_List(user_id=login_user.get('id'),
+                                    share='no')
+>>>>>>> Changed DB and included blocked list
             temp_list = list()
-            for id_ in getids:
+        for id_ in getids:
+            if id_ not in temp_list:
                 temp_list.append(id_)
-            user_obj.blocked_users = json.dumps(temp_list)
-            user_obj.save()
+        user_obj.blocked_users = temp_list
+        user_obj.save()
     data = dict()
     blocked_ids = list()
     response = twit.blocks.ids()
     getids = response.get('ids')
     try:
+<<<<<<< HEAD
         user_obj = Blocked_List.objects.get(user_id=login_id)
         temp_list = eval(user_obj.blocked_users)
+=======
+        blockedListObj = Blocked_List.objects.get(user_id=login_user.get('id'))
+        temp_list = eval(blockedListObj.blocked_users)
+>>>>>>> Changed DB and included blocked list
         for id_ in getids:
             if id_ not in temp_list:
                 temp_list.append(id_)
-        user_obj.blocked_users = json.dumps(temp_list)
-        user_obj.save()
+        blockedListObj.blocked_users = temp_list
     except:
         for id_ in getids:
             blocked_ids.append(id_)
+<<<<<<< HEAD
         blocked_list = Blocked_List(user_id=login_id,
+=======
+        blockedListObj = Blocked_List(user_id=login_user.get('id'),
+>>>>>>> Changed DB and included blocked list
                                         share='no',
-                                        blocked_users=json.dumps(blocked_ids))
-        blocked_list.save()
+                                        blocked_users=blocked_ids)
+    blockedListObj.save()
     response = twit.followers.list()
     getids = response.get('users')
     followers = list()
@@ -157,6 +174,7 @@ def blocked_lists(request):
     token = oauth.OAuthToken.from_string(access_token)   
     twit = Twitter(auth=OAuth(token.key, token.secret,
                 CONSUMER_KEY, CONSUMER_SECRET))
+<<<<<<< HEAD
     data = dict()
     blocked_data = dict()
     login_id = request.session.get('login_id')
@@ -194,18 +212,69 @@ def blocked_lists(request):
                                                subscribed_users=json.dumps(user_list))
             subcribedListObj.save()
     getids = twit.friends.ids(user_id=login_id).get('ids')
+=======
+    login_user = twit.users.show(screen_name=twit.account.settings().get('screen_name'))
+    if request.method == "POST":
+        getids = request.POST.getlist('blocked_users') 
+        user_id = int(request.POST.getlist('user_list')[0])
+        temp_list = list()
+        try:
+            import pdb
+            pdb.set_trace()
+            user_obj = Blocked_List.objects.get(user_id=login_user.get('id'))
+            if not user_obj.subscribed_users == '':
+                subscribed_users = eval(user_obj.subscribed_users)
+            else:
+                subscribed_users = list()
+            if not user_obj.blocked_users == '':
+                temp_list = eval(user_obj.blocked_users)
+            else:
+                temp_list = list()
+            if user_id in subscribed_users:
+                subscribed_users.remove(user_id)
+                for id_ in getids:
+                    response = twit.blocks.destroy(user_id=id_)
+                    if id_ in temp_list:
+                        temp_list.remove(int(id_))
+            else:
+                subscribed_users.append(user_id)
+                for id_ in getids:
+                    response = twit.blocks.create(user_id=id_)
+                    if not int(id_) in temp_list:
+                        temp_list.append(int(id_))
+            user_obj.blocked_users = temp_list
+            user_obj.subscribed_users = subscribed_users
+        except:
+            for id_ in getids:
+                response = twit.blocks.create(user_id=id_)
+                temp_list.append(int(id_))
+            user_obj = Blocked_List(user_id=login_user.get('id'), 
+                                    share='no', 
+                                    blocked_users=temp_list,
+                                    subscribed_users=[user_id])
+        user_obj.save()
+    data = dict()
+    blocked_data = dict()
+    getids = twit.friends.ids(user_id=login_user.get('id')).get('ids')
+>>>>>>> Changed DB and included blocked list
     #check the ids in db for loop
     for id_ in getids:
         try:
-            user_obj = Blocked_List.objects.get(user_id=id_)
+            user_obj = Blocked_List.objects.get(user_id=int(id_))
             friend = twit.users.show(user_id=id_)
             if (user_obj.share == 'no') and (eval(user_obj.blocked_users) != []):
                 #append to data
                 friend_blocks = list()
                 try:
+<<<<<<< HEAD
                     subcribedListObj = Subscribed_List.objects.get(user_id=login_id)
                     if user_obj.user_id in eval(subcribedListObj.subscribed_users):
                         alredy_subscribed = False
+=======
+                    loggedUserBlockObj = Blocked_List.objects.get(user_id=int(login_user.get('id')))
+                    if int(user_obj.user_id) in eval(loggedUserBlockObj.subscribed_users):
+                        alredy_subscribed = True
+>>>>>>> Changed DB and included blocked list
                     else:
                         alredy_subscribed = False
                 except:
